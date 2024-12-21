d43bf7c97df10df7dd1ae71a0eb2e57b5ab3d1ac
crodic <alice01422@gmail.com>
2024-12-21 14:19:39 +0700

    prettier-tailwind

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,19 +1,24 @@
-import { sum } from '@/utils/sum';
-import { useState } from 'react';
+import { sum } from '@/utils/sum'
+import { useState } from 'react'
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
     const handleClick = () => {
-        setCount(sum(count, 1));
-    };
+        setCount(sum(count, 1))
+    }
 
     return (
         <>
             <h1>Counter</h1>
-            <button onClick={handleClick}>Count is: {count}</button>
+            <button
+                className="px-4 py-2"
+                onClick={handleClick}
+            >
+                Count is: {count}
+            </button>
         </>
-    );
-};
+    )
+}
 
-export default Counter;
+export default Counter
